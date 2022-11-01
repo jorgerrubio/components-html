f8ba1fe9962e28f756295706856a8661ccdf2dd8
Jorge Rubio <jorgerrubio@gmail.com>
2022-11-01 16:29:34 +0100

    feat: add responsive cards

---
+++ responsive/script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("change_theme")
+btn.addEventListener("click", (e) => {
+    const cardContent = document.getElementsByClassName("card").item(0)
+    const classList = cardContent.classList
+    classList.toggle("card__flex")
+    classList.toggle("card__grid")
+    const span = btn.getElementsByTagName('span').item(0)
+    span.innerText = classList.value.includes('flex') ? 'Grid' : 'Flex'
+})
